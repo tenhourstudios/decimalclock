b6c5e699aec51916d051e5644b915ca808d2f39d
Pranshu Gaba <pranshugaba@gmail.com>
2020-12-30 20:14:55 +0530

    Implement ViewModel

--- app/src/main/java/com/tenhourstudios/decimalclock/screens/about/AboutFragment.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/screens/about/AboutFragment.kt
@@ -12,6 +12,9 @@ import androidx.lifecycle.ViewModelProvider
 import com.tenhourstudios.decimalclock.R
 import com.tenhourstudios.decimalclock.databinding.FragmentAboutBinding
 
+/**
+ * Shows app version, description, and support links
+ */
 class AboutFragment : Fragment() {
 
     private lateinit var viewModel: AboutViewModel
@@ -29,23 +32,21 @@ class AboutFragment : Fragment() {
         binding.aboutViewModel = viewModel
         binding.lifecycleOwner = this
 
+        binding.aboutTwitterLink.setOnClickListener {
+            viewModel.twitterLink.observe(viewLifecycleOwner, { link -> openUrl(getString(link)) })
+        }
+        binding.aboutEmailId.setOnClickListener {
+            viewModel.emailId.observe(viewLifecycleOwner, { link -> openUrl(getString(link)) })
+        }
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val twitterLink = binding.aboutTwitterLink
-        val emailId = binding.aboutEmailId
-        twitterLink.setOnClickListener {
-            openUrl(getString(R.string.twitter_link))
-        }
-        emailId.setOnClickListener {
-            openUrl(getString(R.string.email_id))
-        }
-    }
 
+    /**
+     * Opens the Url
+     */
     private fun openUrl(url: String) {
-        val i = Intent(Intent.ACTION_VIEW)
-        i.data = Uri.parse(url)
-        startActivity(i)
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }
--- app/src/main/java/com/tenhourstudios/decimalclock/screens/about/AboutViewModel.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/screens/about/AboutViewModel.kt
@@ -4,15 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tenhourstudios.decimalclock.BuildConfig
+import com.tenhourstudios.decimalclock.R
 
 class AboutViewModel : ViewModel() {
     private val _appVersionText = MutableLiveData<String>()
     val appVersionText: LiveData<String>
         get() = _appVersionText
 
+    private val _twitterLink = MutableLiveData<Int>()
+    val twitterLink: LiveData<Int>
+        get() = _twitterLink
+
+    private val _emailId = MutableLiveData<Int>()
+    val emailId: LiveData<Int>
+        get() = _emailId
+
     init {
         _appVersionText.value = BuildConfig.VERSION_NAME
+        _twitterLink.value = R.string.twitter_link
+        _emailId.value = R.string.email_id
     }
-
 
 }
