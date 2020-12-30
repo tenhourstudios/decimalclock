afc02f96240b270e656ddd2e4ca8b0cc00254cf2
Pranshu Gaba <pranshugaba@gmail.com>
2020-12-31 02:14:02 +0530

    Add viewmodel to ClockFragment.kt

--- app/src/main/java/com/tenhourstudios/decimalclock/screens/clock/ClockFragment.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/screens/clock/ClockFragment.kt
@@ -1,126 +1,66 @@
 package com.tenhourstudios.decimalclock.screens.clock
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.preference.PreferenceManager
 import com.tenhourstudios.decimalclock.R
 import com.tenhourstudios.decimalclock.data.*
-import com.tenhourstudios.decimalclock.data.clock.Clock
 import com.tenhourstudios.decimalclock.databinding.FragmentClockBinding
-import timber.log.Timber
-import java.time.Instant
-import java.time.OffsetTime
 
 
 class ClockFragment : Fragment() {
-    private var _binding: FragmentClockBinding? = null
 
-    private val binding get() = _binding!!
-
-    private val separator = ":"
-    var tenSeparator = separator
-    var twentyFourSeparator = separator
-
-    // preferences initialization
-    private var displaySeconds: Boolean = false
-    var blinkingSeparator = false
-    var precision = KEY_PRECISION_LOW
-    var format: String = KEY_STANDARD
-    var updateFrequency: Long = 1000
-
-    // get the devices timezone
-    private val timeZoneOffset = OffsetTime.now().offset
-
-
-    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var viewModel: ClockViewModel
+    private lateinit var binding: FragmentClockBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        Timber.d(timeZoneOffset.toString())
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentClockBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
-    override fun onResume() {
-        super.onResume()
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_clock, container, false)
+        viewModel = ViewModelProvider(this).get(ClockViewModel::class.java)
+
+
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        displaySeconds = sharedPrefs.getBoolean("display_seconds_preference", false)
-        blinkingSeparator = sharedPrefs.getBoolean("blinking_separator_preference", false)
-        format = when(sharedPrefs.getString("preference_format_digital", getString(R.string.prefs_digital_standard)))
-        {
+        //viewModel.blinkingSeparator = sharedPrefs.getBoolean("blinking_separator_preference", false)
+        viewModel.format = when (sharedPrefs.getString(
+            "preference_format_digital",
+            getString(R.string.prefs_digital_standard)
+        )) {
             getString(R.string.prefs_digital_decimal) -> KEY_DECIMAL
             getString(R.string.prefs_digital_percentage) -> KEY_PERCENTAGE
             else -> KEY_STANDARD
         }
-        precision = when(sharedPrefs.getString("preference_precision_digital", getString(R.string.prefs_precision_low)))
-        {
+        viewModel.precision = when (sharedPrefs.getString(
+            "preference_precision_digital",
+            getString(R.string.prefs_precision_low)
+        )) {
             getString(R.string.prefs_precision_medium) -> KEY_PRECISION_MEDIUM
             getString(R.string.prefs_precision_high) -> KEY_PRECISION_HIGH
             else -> KEY_PRECISION_LOW
         }
-        updateFrequency = when (precision) {
+        viewModel.updateFrequency = when (viewModel.precision) {
             KEY_PRECISION_MEDIUM -> 16
             KEY_PRECISION_HIGH -> 4
             else -> 1000
         }
-        startUpdatingTime()
-    }
 
-    private val updateTime = object : Runnable {
+        binding.clockViewModel = viewModel
+        binding.lifecycleOwner = this
 
-        override fun run() {
-            // get time in milliseconds since Unix epoch
-            val millisSinceEpoch = Instant.now().toEpochMilli()
-
-            // add offset and mod to get milliseconds since last midnight
-            val millisToday =
-                (millisSinceEpoch + 1000 * timeZoneOffset.totalSeconds) % MILLIS_IN_A_DAY
-
-            val time = Clock(millisToday)
-            binding.tenHourTime.text = time.tenHourTime(format, precision)
-            /*
-            val tClock = TenHourClock(millisToday)
-            tenSeparator = when (blinkingSeparator && (tClock.getSecond() % 2 == 0)) {
-                true -> " "
-                false -> separator
-            }
-            twentyFourSeparator = when (blinkingSeparator && (time.twentyFourSecond % 2 == 0)) {
-                true -> " "
-                false -> separator
-            }
-
- */
-            binding.twentyFourHourTime.text =
-                time.twentyFourHourTime(precision)
-
-            handler.postDelayed(this, updateFrequency)
-        }
-    }
-
-    override fun onStop() {
-        stopUpdatingTime()
-        super.onStop()
-    }
-
-    private fun startUpdatingTime() {
-        updateTime.run()
-    }
-
-    private fun stopUpdatingTime() {
-        handler.removeCallbacks(updateTime)
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- app/src/main/java/com/tenhourstudios/decimalclock/screens/clock/ClockViewModel.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/screens/clock/ClockViewModel.kt
@@ -1,7 +1,67 @@
 package com.tenhourstudios.decimalclock.screens.clock
 
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.tenhourstudios.decimalclock.data.KEY_PRECISION_LOW
+import com.tenhourstudios.decimalclock.data.KEY_STANDARD
+import com.tenhourstudios.decimalclock.data.MILLIS_IN_A_DAY
+import com.tenhourstudios.decimalclock.data.clock.Clock
+import java.time.Instant
+import java.time.OffsetTime
 
 class ClockViewModel : ViewModel() {
 
+    // preferences initialization
+    var displaySeconds: Boolean = false
+    var blinkingSeparator = false
+    var precision = KEY_PRECISION_LOW
+    var format: String = KEY_STANDARD
+    var updateFrequency: Long = 2
+
+    // get the devices timezone
+    private val timeZoneOffset = OffsetTime.now().offset
+
+    private val _clock = MutableLiveData<Clock>()
+    val clock: LiveData<Clock>
+        get() = _clock
+
+    val tenHourTime: LiveData<String> =  Transformations.map(clock) {
+        currentClock ->
+        currentClock.tenHourTime(format, precision)
+    }
+
+    val twentyFourHourTime: LiveData<String> =  Transformations.map(clock) {
+            currentClock ->
+        currentClock.twentyFourHourTime(precision)
+    }
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val updateTime = object : Runnable {
+        override fun run() {
+            // get time in milliseconds since Unix epoch
+            val millisSinceEpoch = Instant.now().toEpochMilli()
+
+            // add offset and mod to get milliseconds since last midnight
+            val millisToday =
+                (millisSinceEpoch + 1000 * timeZoneOffset.totalSeconds) % MILLIS_IN_A_DAY
+
+            _clock.value = Clock(millisToday)
+
+            handler.postDelayed(this, updateFrequency)
+        }
+    }
+
+    init {
+        updateTime.run()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        handler.removeCallbacks(updateTime)
+    }
 }
