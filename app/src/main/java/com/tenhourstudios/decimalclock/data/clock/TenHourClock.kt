338a2f058b3ab8b15bf3dff74fb3d42110788495
Pranshu Gaba <pranshugaba@gmail.com>
2020-12-31 00:55:24 +0530

    Fix bug in ten hour clock

--- app/src/main/java/com/tenhourstudios/decimalclock/data/clock/TenHourClock.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/data/clock/TenHourClock.kt
@@ -4,26 +4,62 @@ import com.tenhourstudios.decimalclock.data.KEY_PRECISION_HIGH
 import com.tenhourstudios.decimalclock.data.KEY_PRECISION_MEDIUM
 import com.tenhourstudios.decimalclock.data.MILLIS_IN_A_DAY
 
-
+/**
+ * This class computes the Ten Hour time given the milliseconds elapsed in the day.
+ */
 class TenHourClock(millis: Long) {
 
-    // total milliseconds elapsed today
     private val dMillis = millis * 100000000 / MILLIS_IN_A_DAY
 
-    // total seconds elapsed today rounded down
+    /** total seconds elapsed today rounded down */
     private val dSecond = dMillis / 1000
 
-    // total minutes elapsed today rounded down
+    /** total minutes elapsed today rounded down */
     private val dMinute = dSecond / 100
 
-    // total hours elapsed today rounded down
+    /** total hours elapsed today rounded down */
     private val dHour = dMinute / 100
 
-    private fun getMillisecondPadded(width: Int = 2): String {
-        return (dMillis/10 % 100).toString().padStart(width, '0')
+    /**
+     * Formats the decimal time in hours, minutes and seconds
+     * @return a string of the form h:mm:ss:uu
+     */
+    fun getStandard(precision: Int): String {
+        var standardTime = "$dHour:${this.getMinutePadded()}"
+        if (precision >= KEY_PRECISION_MEDIUM) {
+            standardTime += ":${this.getSecondPadded()}"
+        }
+        if (precision >= KEY_PRECISION_HIGH) {
+            standardTime += ":${this.getCentisecondPadded()}"
+        }
+        return standardTime
     }
 
-    // returns seconds between 0 and 99 inclusive
+    /**
+     * Formats the decimal time as a decimal between 0 and 10.
+     * @return a string of the form h.mmssuu
+     */
+    fun getDecimal(precision: Int): String {
+        return "$dHour." + this.getMillisecondPadded().slice(1 until precision)
+    }
+
+    fun getPercentage(precision: Int): String {
+        return "${dMinute / 10}." + this.getMillisecondPadded().slice(2 until precision) + "%"
+    }
+
+
+    private fun getMillisecondPadded(width: Int = 8): String {
+        return dMillis.toString().padStart(width, '0')
+    }
+
+    private fun getCentisecondPadded(width: Int = 2): String {
+        return ((dMillis / 10) % 100).toString().padStart(width, '0')
+    }
+
+    /**
+     * Computes seconds elapsed in the current minute
+     * @return an integer between 0 and 99 inclusive
+     */
     private fun getSecond(): Int {
         return (dSecond % 100).toInt()
     }
@@ -32,7 +68,10 @@ class TenHourClock(millis: Long) {
         return this.getSecond().toString().padStart(width, '0')
     }
 
-    // returns minutes between 0 and 99 inclusive
+    /**
+     * Computes minutes elapsed in the current hour
+     * @return an integer between 0 and 99 inclusive
+     */
     private fun getMinute(): Int {
         return (dMinute % 100).toInt()
     }
@@ -41,7 +80,10 @@ class TenHourClock(millis: Long) {
         return this.getMinute().toString().padStart(width, '0')
     }
 
-    // returns hours between 0 and 9 inclusive
+    /**
+     * Computes hours elapsed in the current day
+     * @return an integer between 0 and 9 inclusive
+     */
     private fun getHour(): Int {
         return (dHour % 10).toInt()
     }
@@ -50,21 +92,4 @@ class TenHourClock(millis: Long) {
         return this.getHour().toString().padStart(width, '0')
     }
 
-    fun  getStandard(precision: Int): String{
-        var standardTime = "${this.getHourPadded()}:${this.getMinutePadded()}"
-        if (precision >= KEY_PRECISION_MEDIUM) {
-            standardTime += ":${this.getSecondPadded()}"
-        }
-        if (precision >= KEY_PRECISION_HIGH) {
-            standardTime += ":${this.getMillisecondPadded()}"
-        }
-        return standardTime
-    }
-    fun getDecimal(precision: Int): String{
-        return "${this.getHourPadded()}." + dMillis.toString().slice(1 until precision)
-    }
-
-    fun getPercentage(precision: Int): String{
-        return "${dMinute/10}." + dMillis.toString().slice(2 until precision) + "%"
-    }
 }
