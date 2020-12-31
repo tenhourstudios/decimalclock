f4d180ece1f76326619cbf594b2731c274e73835
Pranshu Gaba <pranshugaba@gmail.com>
2020-12-31 17:20:58 +0530

    Refactor 24 hour clock

--- app/src/main/java/com/tenhourstudios/decimalclock/data/clock/Clock.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/data/clock/Clock.kt
@@ -2,11 +2,9 @@ package com.tenhourstudios.decimalclock.data.clock
 
 import com.tenhourstudios.decimalclock.data.KEY_DECIMAL
 import com.tenhourstudios.decimalclock.data.KEY_PERCENTAGE
-import com.tenhourstudios.decimalclock.data.KEY_PRECISION_HIGH
-import com.tenhourstudios.decimalclock.data.KEY_PRECISION_MEDIUM
 
 
-class Clock(private val millis: Long) {
+class Clock(millis: Long) {
 
     private val dClock = TenHourClock(millis)
 
@@ -18,21 +16,10 @@ class Clock(private val millis: Long) {
         }
     }
 
-    val twentyFourSecond = ((millis / 1000) % 60).toInt()
-    val twentyFourMinute = (millis / (60 * 1000)) % 60
-    val twentyFourHour = (millis / (60 * 60 * 1000))
+    private val tfClock = TwentyFourHourClock(millis)
 
     fun twentyFourHourTime(precision: Int): String {
-        var twentyFourHourTime = "${twentyFourHour.toString().padStart(2, '0')}:${
-            twentyFourMinute.toString().padStart(2, '0')
-        }"
-        if (precision >= KEY_PRECISION_MEDIUM) {
-            twentyFourHourTime += ":${twentyFourSecond.toString().padStart(2, '0')}"
-        }
-        if (precision >= KEY_PRECISION_HIGH) {
-            twentyFourHourTime += ":" + "${millis /10 % 100}".padStart(2, '0')
-        }
-        return twentyFourHourTime
+        return tfClock.twentyFourHourTime(precision)
     }
 
 }
--- app/src/main/java/com/tenhourstudios/decimalclock/data/clock/TwentyFourHourClock.kt
+++ app/src/main/java/com/tenhourstudios/decimalclock/data/clock/TwentyFourHourClock.kt
@@ -1,4 +1,25 @@
 package com.tenhourstudios.decimalclock.data.clock
 
-class TwentyFourHourClock {
+import com.tenhourstudios.decimalclock.data.KEY_PRECISION_HIGH
+import com.tenhourstudios.decimalclock.data.KEY_PRECISION_MEDIUM
+
+class TwentyFourHourClock(private val millis: Long) {
+
+    private val twentyFourSecond = ((millis / 1000) % 60).toInt()
+    private val twentyFourMinute = (millis / (60 * 1000)) % 60
+    private val twentyFourHour = (millis / (60 * 60 * 1000))
+
+    fun twentyFourHourTime(precision: Int): String {
+        var twentyFourHourTime = "${twentyFourHour.toString().padStart(2, '0')}:${
+            twentyFourMinute.toString().padStart(2, '0')
+        }"
+        if (precision >= KEY_PRECISION_MEDIUM) {
+            twentyFourHourTime += ":${twentyFourSecond.toString().padStart(2, '0')}"
+        }
+        if (precision >= KEY_PRECISION_HIGH) {
+            twentyFourHourTime += ":" + "${millis / 10 % 100}".padStart(2, '0')
+        }
+        return twentyFourHourTime
+    }
+
 }
